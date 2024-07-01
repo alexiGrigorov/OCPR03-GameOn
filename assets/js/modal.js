2ccbba0e558fd90871936b5c4df7531d853c2462
Alexi GRIGOROV <alexigrigorov@gmail.com>
2024-07-01 12:32:50 +0200

    fixed the header with the navigation and the footer

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,10 +1,6 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+  const siteHeader = document.getElementById("mainNavbar");
+  siteHeader.classList.toggle("expanded-main-navbar");
 }
 
 // DOM Elements
