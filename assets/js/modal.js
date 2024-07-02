87c3b090f55cfddf1fd7051f2d4d3222b9cf5269
Alexi GRIGOROV <alexigrigorov@gmail.com>
2024-07-02 16:44:32 +0200

    finished fixing the design, and started the object oriented solution for the form

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,17 +1,57 @@
-function editNav() {
-  const siteHeader = document.getElementById("mainNavbar");
-  siteHeader.classList.toggle("expanded-main-navbar");
+/********** Classes **********/
+
+/********** App Features **********/
+
+class App {
+  constructor(expendableNavbar, formModal) {
+    this.expendableNavbar = expendableNavbar;
+    this.formModal = formModal;
+  }
+  init() {
+    Object.values(this).forEach((feature) => {
+      feature.init();
+    });
+  }
 }
+
+/********** App Features **********/
+
+class ExpendableNavbar {
+  constructor(navElement) {
+    this.nav = navElement;
+    this.menuBtn = this.nav.lastElementChild;
+  }
+  toggleExpanded() {
+    console.log("clicked");
+    this.nav.classList.toggle("expanded");
+  }
+  init() {
+    this.menuBtn.addEventListener("click", () => this.toggleExpanded());
+  }
+}
+
+class Modal {
+  constructor(dialogElement, openBtn) {
+    this.dialog = dialogElement;
+    this.openBtn = openBtn;
+    this.closeBtn = dialogElement.firstElementChild;
+  }
+  init() {
+    console.log("Modal connected");
+    this.openBtn.addEventListener("click", () => this.dialog.showModal());
+    this.closeBtn.addEventListener("click", () => this.dialog.close());
+  }
+}
+
+const siteHeader = new ExpendableNavbar(document.getElementById("mainNavbar"));
+const signupModal = new Modal(
+  document.getElementsByClassName("signup")[0],
+  document.getElementsByClassName("btn-signup")[0]
+);
+const app = new App(siteHeader, signupModal);
+app.init();
+app.formModal.dialog.showModal();
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+
 const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
