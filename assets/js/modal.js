69889c7420867314d7107fb6658402fd03e63d4c
Alexi GRIGOROV <alexigrigorov@gmail.com>
2024-07-03 03:14:35 +0200

    finished most of the structure of the app

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,11 +1,12 @@
 /********** Classes **********/
 
-/********** App Features **********/
+/********** App **********/
 
 class App {
-  constructor(expendableNavbar, formModal) {
+  constructor(expendableNavbar, modal, validatableForm) {
     this.expendableNavbar = expendableNavbar;
-    this.formModal = formModal;
+    this.modal = modal;
+    this.validatableForm = validatableForm;
   }
   init() {
     Object.values(this).forEach((feature) => {
@@ -26,9 +27,12 @@ class ExpendableNavbar {
     this.nav.classList.toggle("expanded");
   }
   init() {
+    console.log("ExpendableNavbar connected");
     this.menuBtn.addEventListener("click", () => this.toggleExpanded());
   }
 }
+
+/*****/
 
 class Modal {
   constructor(dialogElement, openBtn) {
@@ -43,14 +47,82 @@ class Modal {
   }
 }
 
-const siteHeader = new ExpendableNavbar(document.getElementById("mainNavbar"));
-const signupModal = new Modal(
-  document.getElementsByClassName("signup")[0],
-  document.getElementsByClassName("btn-signup")[0]
+/*****/
+
+class ValidatableForm {
+  constructor(formElement, submitBtn) {
+    this.form = formElement;
+    if (!this.form.contains(submitBtn)) {
+      throw new Error(
+        "The provided submit button is not associated with this form"
+      );
+    }
+    this.submitBtn = submitBtn;
+  }
+  init() {
+    console.log("ValidatableForm connected");
+  }
+
+  validateForm() {}
+}
+
+/********** Validatable Form Elements **********/
+
+class ValidatableTextInput {
+  constructor(inputTextElement, formDataElement) {
+    if (inputTextElement.type !== "text") {
+      throw new Error("The provided element is not a text input");
+    }
+    this.input = inputTextElement;
+
+    if (!formDataElement.classList.contains("formData")) {
+      throw new Error("The provided element is not formData input wrapper");
+    }
+    if (!formDataElement.contains(this.input)) {
+      throw new Error(
+        "The provided element is not the appropriate formData input wrapper for the input element"
+      );
+    }
+
+    this.formData = formDataElement;
+
+    // this.formData.dataset.errorVisible = "false";
+  }
+
+  init() {
+    console.log(`${this.input.id} ValidatableTextInput is connected`);
+  }
+  validate() {}
+}
+
+//
+
+const first = new ValidatableTextInput(
+  document.getElementById("first"),
+  document.getElementById("first").parentElement
 );
-const app = new App(siteHeader, signupModal);
+first.init();
+
+//
+
+/********** Instantiating the features **********/
+const siteHeader = new ExpendableNavbar(document.getElementById("mainNavbar"));
+
+const signupModal = new Modal(
+  document.getElementById("signup"),
+  document.getElementById("btn-signup")
+);
+
+const form = new ValidatableForm(
+  document.forms["reserve"],
+  document.getElementById("btn-submit")
+  // test for the constructor validation -> document.getElementById("btn-signup")
+);
+
+/********** Instantiating the app **********/
+const app = new App(siteHeader, signupModal, form);
 app.init();
-app.formModal.dialog.showModal();
+app.modal.dialog.showModal();
 
 // DOM Elements
 
