e8c3a22891bb43d3bbb38f2484afcb7faf054d5e
Alexi GRIGOROV <alexigrigorov@gmail.com>
2024-07-04 15:40:37 +0200

    made the select fields tabable and started implementing the validation algorithms

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -20,7 +20,7 @@ class ExpendableNavbar {
   constructor(NAV) {
     this.navBar = NAV;
     this.menuBtn = Array.from(this.navBar.children).filter(
-      (el) => el.tagName === "I"
+      (el) => el.tagName === "BUTTON"
     )[0];
   }
   toggleExpanded() {
@@ -50,7 +50,7 @@ class Modal {
     this.dialog = DIALOG;
     this.openBtn = BUTTON;
     this.closeBtn = Array.from(this.dialog.children).filter(
-      (el) => el.tagName === "I"
+      (el) => el.tagName === "BUTTON"
     )[0];
   }
   init() {
@@ -70,23 +70,39 @@ class ValidatableForm {
     this.validationBlocksArray = validationBlocksArray;
   }
   init() {
-    console.log("ValidatableForm connected");
+    this.submitBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      this.validateForm();
+    });
+    this.validationBlocksArray.forEach((validationBlock) => {
+      validationBlock.init();
+    });
   }
 
-  validateForm() {}
+  validateForm() {
+    this.validationBlocksArray.forEach((block) => block.validateBlock());
+  }
 }
 
 /****/
 
 class ValidationBlock {
-  constructor(DIV, validatableInputsArray) {
-    this.formData = DIV;
+  constructor(FIELDSET, validatableInputsArray) {
+    this.formData = FIELDSET;
     this.validatableInputsArray = validatableInputsArray;
   }
   init() {
-    console.log("ValidationBlock connected");
+    this.validatableInputsArray.forEach((input) => input.init());
   }
-  validateBlock() {}
+  validateBlock() {
+    delete this.formData.dataset.error;
+    try {
+      this.validatableInputsArray.forEach((input) => input.validateInput());
+    } catch (error) {
+      this.formData.dataset.error = error.message;
+    }
+  }
 }
 
 /**/
@@ -94,13 +110,23 @@ class ValidationBlock {
 class ValidatableInputText {
   constructor(INPUT) {
     this.input = INPUT;
+    this.label = this.input.previousElementSibling;
   }
 
-  init() {
-    this.input.addEventListener("blur", () => this.validate());
-  }
-  validate() {
-    console.log(`${this.input.id} validated`);
+  init() {}
+
+  validateInput() {
+    if (this.input.validity.valid) return;
+
+    if (this.input.validity.valueMissing)
+      throw new Error(
+        `Veuillez remplir votre ${this.label.innerText.toLowerCase()}.`
+      );
+
+    if (this.input.validity.tooShort)
+      throw new Error(
+        `Veuillez entrer 2 caractères ou plus pour le champ du ${this.label.innerText.toLowerCase()}.`
+      );
   }
 }
 
@@ -109,11 +135,15 @@ class ValidatableInputEmail {
     this.input = INPUT;
   }
 
-  init() {
-    this.input.addEventListener("blur", () => this.validate());
-  }
-  validate() {
-    console.log(`${this.input.id} validated`);
+  init() {}
+  validateInput() {
+    if (this.input.validity.valid) return;
+
+    if (this.input.validity.valueMissing)
+      throw new Error(`Veuillez remplir votre email.`);
+
+    if (this.input.validity.typeMismatch)
+      throw new Error(`Veuillez saisir un adresse email valide`);
   }
 }
 
@@ -122,12 +152,8 @@ class ValidatableInputDate {
     this.input = INPUT;
   }
 
-  init() {
-    this.input.addEventListener("blur", () => this.validate());
-  }
-  validate() {
-    console.log(`${this.input.id} validated`);
-  }
+  init() {}
+  validateInput() {}
 }
 
 class ValidatableInputNumber {
@@ -135,51 +161,112 @@ class ValidatableInputNumber {
     this.input = INPUT;
   }
 
-  init() {
-    this.input.addEventListener("blur", () => this.validate());
-  }
-  validate() {
-    console.log(`${this.input.id} validated`);
+  init() {}
+  validateInput() {
+    // Custom validation that is outside the scope of the Constraint Validation API
+    const onlyDigits = /^-?\d+(\.\d+)?$/;
+    if (!onlyDigits.test(this.input.value))
+      throw new Error("Veuillez saisir une valeur numérique en chiffres.");
+
+    if (this.input.validity.valid) return;
+
+    if (this.input.validity.rangeUnderflow)
+      throw new Error(
+        `Il y a toujours une première fois, mais jamais de fois négative`
+      );
+
+    if (this.input.validity.rangeOverflow)
+      throw new Error(`Nous n'avons même pas organisé autant d'événements`);
   }
 }
 
 class ValidatableInputRadio {
   constructor(INPUT) {
     this.input = INPUT;
+    this.label = this.input.nextElementSibling;
   }
 
   init() {
-    this.input.addEventListener("blur", () => this.validate());
+    // this.input.addEventListener("blur", () => this.validate());
+    this.label.addEventListener("keyup", (e) => {
+      if (e.keyCode === 32) this.input.click();
+    });
   }
-  validate() {
-    console.log(`${this.input.id} validated`);
-  }
+  validateInput() {}
 }
 
 class ValidatableInputCheckbox {
   constructor(INPUT) {
     this.input = INPUT;
+    this.label = this.input.nextElementSibling;
   }
 
   init() {
-    this.input.addEventListener("blur", () => this.validate());
+    // this.input.addEventListener("blur", () => this.validate());
+    this.label.addEventListener("keyup", (e) => {
+      console.log(e.target);
+      if (e.keyCode === 32) this.input.click();
+    });
   }
-  validate() {
-    console.log(`${this.input.id} validated`);
+  validateInput() {
+    if (this.input.validity.valid) return;
+
+    console.log(this.input.validity);
+    if (this.input.validity.valueMissing)
+      throw new Error(
+        `Veuillez remplir votre ${this.label.innerText.toLowerCase()}.`
+      );
   }
 }
 
-/********** Instantiating the features **********/
-const siteHeader = new ExpendableNavbar(document.getElementById("mainNavbar"));
+/********** Instantiating the Validation blocks **********/
 
-const signupModal = new Modal(
+const first = new ValidatableInputText(document.getElementById("first"));
+const last = new ValidatableInputText(document.getElementById("last"));
+const email = new ValidatableInputEmail(document.getElementById("email"));
+const birthdate = new ValidatableInputDate(
+  document.getElementById("birthdate")
+);
+const quantity = new ValidatableInputNumber(
+  document.getElementById("quantity")
+);
+const locations = Array.from(document.getElementsByName("location")).map(
+  (el) => new ValidatableInputRadio(el)
+);
+const terms = new ValidatableInputCheckbox(document.getElementById("terms"));
+const interested = new ValidatableInputCheckbox(
+  document.getElementById("interested")
+);
+
+const formValidationBlocks = [first, last, email, birthdate, quantity].map(
+  (field) => new ValidationBlock(field.input.parentNode, [field])
+);
+formValidationBlocks.push(
+  new ValidationBlock(locations[0].input.parentNode, locations)
+); // location is an array of radio buttons
+formValidationBlocks.push(
+  new ValidationBlock(document.getElementsByClassName("conditions")[0], [
+    terms,
+    interested,
+  ])
+); // location is an array of radio buttons
+
+/********** Instantiating the features **********/
+const mainNavbar = new ExpendableNavbar(document.getElementById("mainNavbar"));
+
+const signup = new Modal(
   document.getElementById("signup"),
   document.getElementById("btn-signup")
 );
-const reserve = new ValidatableForm(document.forms["reserve"], []);
-const signup = new ModalWithValidatableForm(signupModal, reserve);
+const reserve = new ValidatableForm(
+  document.forms["reserve"],
+  formValidationBlocks
+);
+const signupModalForm = new ModalWithValidatableForm(signup, reserve);
 
 /********** Instantiating the app **********/
-const app = new App(siteHeader, signup);
+const app = new App(mainNavbar, signupModalForm);
+console.log(app);
+
 app.init();
 app.modalWithValidatableForm.modal.dialog.showModal();
